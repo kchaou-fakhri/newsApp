e1d4135f6f09335d54c5f93e0b57e82cdf7faf16
mobelite-fkchaou <fkchaou@mobelite.fr>
2024-03-13 15:19:06 +0100

    feat(Details Screen) : Implement the webView -Implement the webView inside DetailsScreen #feat /details_screen

--- app/src/main/java/com/dev0kch/learn/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/dev0kch/learn/presentation/navigation/NavGraph.kt
@@ -1,33 +1,35 @@
 package com.dev0kch.learn.presentation.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.dev0kch.learn.domain.model.Article
 import com.dev0kch.learn.presentation.details.DetailsScreen
 import com.dev0kch.learn.presentation.news.NewsScreen
+import com.dev0kch.learn.presentation.viewmodel.SharedViewModel
 
 @Composable
 fun HomeNavigation(startDestination: String) {
+    val sharedViewModel : SharedViewModel = viewModel()
     val navController = rememberNavController()
 
     NavHost(navController = navController, startDestination = startDestination) {
 
         composable(route = Route.HomeScreen.route) {
-            NewsScreen(navController)
+            NewsScreen(navController,sharedViewModel = sharedViewModel)
         }
 
         composable(route = Route.DetailsScreen.route) {
-            val result =
-                navController
-                    .previousBackStackEntry
-                    ?.savedStateHandle
-                    ?.get<Article>(Configuration.ARTICLE_PARAM)
+//            val result =
+//                navController
+//                    .previousBackStackEntry
+//                    ?.savedStateHandle
+//                    ?.get<Article>(Configuration.ARTICLE_PARAM)
 
 
 
-            DetailsScreen(navController, result!!)
+            DetailsScreen(navController, sharedViewModel = sharedViewModel)
         }
     }
 }
